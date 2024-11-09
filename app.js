f07e5ffba243925c52b67c0fa2995e819ff266d3
saket singh <saketchaudhary2004@gmail.com>
2024-11-09 22:48:47 +0530

    todays commit

---
+++ app.js
@@ -0,0 +1,8 @@
+// let smallImages = document.getElementsByClassName("oldImg");
+
+// for(let i =0;i<smallImages.length;i++){
+//     smallImages[i].src = "spiderman_img.png";
+//     console.log(`this is image no ${i} ${smallImages[i].src}`);
+// };
+
+// console.dir(document.querySelectorAll("p"));
