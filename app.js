7f6258ed262ddfd5760fdbb61de2dd18719c054b
saket singh <saketchaudhary2004@gmail.com>
2024-11-10 10:29:43 +0530

    today's commit

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
-// let smallImages = document.getElementsByClassName("oldImg");
+let body = document.querySelector('body');
 
-// for(let i =0;i<smallImages.length;i++){
-//     smallImages[i].src = "spiderman_img.png";
-//     console.log(`this is image no ${i} ${smallImages[i].src}`);
-// };
+let redP = document.createElement('p');
+redP.innerText = "Hey i am red";
+redP.classList.add('red');
+body.appendChild(redP);
 
-// console.dir(document.querySelectorAll("p"));
+let h3 = document.createElement('h3');
+h3.innerText = "hey i am blue";
+h3.classList.add('blue');
+body.appendChild(h3);
+
+let div = document.createElement('div');
+div.classList.add('box');
+let h1 = document.createElement('h1');
+h1.innerText = "i am in div";
+let p = document.createElement('p');
+p.innerText = "me too ";
+div.appendChild(h1);
+div.appendChild(p);
+
+body.appendChild(div);
